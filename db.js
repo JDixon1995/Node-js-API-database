c882008cd847618e9bafa74b252e738348351a36
John <jddhalogamer@gmail.com>
2021-02-02 16:00:06 -0600

    minor path debugging

--- db.js
+++ db.js
@@ -1,2 +1,7 @@
 const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 mongoose.connect('mongodb+srv:BionicmanAdam:bionicmanadamjensen@cluster0.dpxya.mongodb.net/Cluster0?retryWrites=true&w=majority'); 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const server = app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+const server = app.listen(port, () => {
+    console.log('Express server listening on port ' + port);
 });
